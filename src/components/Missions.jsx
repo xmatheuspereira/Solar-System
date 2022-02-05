686854ea64cae14cc2127cb51efca914abc22bc7
Matheus Pereira <matheusdimmy@hotmail.com>
2022-02-05 16:30:49 -0300

    Limpando erros

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -5,7 +5,7 @@ class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
-        <Title headline="Missões"/>
+        <Title headline="Missões" />
       </div>
     );
   }
