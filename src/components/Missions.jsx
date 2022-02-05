049d87102e1fcd8eda21fe898a7e89c4e853e3c0
Matheus Pereira <matheusdimmy@hotmail.com>
2022-02-05 16:15:55 -0300

    Limpando erros

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions"/>
-    )
+      <div data-testid="missions" />
+    );
   }
 }
 
